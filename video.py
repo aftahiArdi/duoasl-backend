14cdc5d392589b652433cab1de7653b804d8def1
Jason Zhao <jason1382311@gmail.com>
2024-01-20 16:01:49 -0800

    switch port

--- video.py
+++ video.py
@@ -51,4 +51,4 @@ def download_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
